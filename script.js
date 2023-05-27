5f13df77d6f8022db44bcea311ab09510013b8f1
Skanda <sskanda98@gmail.com>
2023-05-27 18:41:52 +0530

    cursor effect

--- script.js
+++ script.js
@@ -3,7 +3,30 @@
 const boxmenu = document.querySelector('.box-menu');
 const navbar = document.querySelector('.navbar');
 const icon = document.querySelector('.fa-solid');
+const cursor = document.querySelector('.cursor');
 
+document.addEventListener('mousemove', function (e) {
+    let x = e.pageX;
+    let y = e.pageY;
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+})
+
+
+
+// window.addEventListener('click', function () {
+//     // if (cursor.classList.contains("cursor-animation"))
+//     // cursor.classList.remove("cursor-animation");
+//     // cursor.classList.add("cursor-animation");
+//     // cursor.classList.remove("cursor-animation");
+//     cursor.style.animation = "ripple-effect 0.4s linear ";
+//     cursor.onanimationend = () => {
+//         cursor.style.animation = "none";
+//     }
+
+//     // cursor.style.background = "red";
+//     console.log("clicked");
+// })
 
 
 boxmenu.addEventListener('click', function () {
